71ff643988a4e47be34ebfd60d2e90fbcaf8c862
ibartel <ingo.bartel@googlemail.com>
2014-07-25 01:52:57 +0200

    added initial config class, added reconnect functionality

--- tunnel-manager.js
+++ tunnel-manager.js
@@ -1,4 +1,8 @@
-var Tunnel = require('./lib/Tunnel');
+var Tunnel = require('./lib/Tunnel'),
+    Config = require('./lib/Config'),
+    Promise = require('bluebird');
+
+path = require('path-extra');
 
 var ssh_config = {
     host: 'my-chaos.net',
@@ -7,24 +11,36 @@ var ssh_config = {
     password: 'test'
 };
 
+var config = new Config();
+
 var tunnelManager = new Tunnel({verbose: true});
 
 tunnelManager.on('log', function(message) {
     console.log("LOG: " + JSON.stringify(message, null, 2));
 });
 
-tunnelManager.connect(ssh_config).done(function(connection) {
-    connection.addTunnel('github', 443, 'github.com', 443).then(function(wasdas) {
-        console.log("tunnel zu github da");
-        console.log(wasdas);
-    }).catch(function(error) {
-        console.error("Error adding tunnel: " + error);
-    });
-//    connection.close().then(function() {
-//        console.log("und tschuess");
-//    }).catch(function(error) {
-//        console.error("Schade schade");
-//    })
-}).catch(function(error) {
-    console.error(error);
+tunnelManager.on('closed', function (data) {
+    console.log(data);
+    tunnelManager.reconnect(data.config, data.tunnel).catch(console.err);
 });
+
+var makeTunnel = function (connection) {
+    var tunnels = [
+        connection.addTunnel('mysql', 3306, 'localhost', 3306).catch(function (err) {
+            console.error(err)
+        }),
+        connection.addTunnel('github', 443, 'github.com', 443).catch(function (err) {
+            console.error(err)
+        })
+    ];
+
+    Promise.all(tunnels).then(function () {
+        console.log("all done");
+    });
+};
+
+tunnelManager.connect(ssh_config)
+    .then(makeTunnel)
+    .catch(function (error) {
+        console.error(error);
+    });
